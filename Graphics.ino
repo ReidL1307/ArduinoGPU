451717dc0bf2338df9b9762c1ccbb1f01bc58636
ReidL1307 <79946812+ReidL1307@users.noreply.github.com>
2022-08-23 18:07:38 -0600

    Updated keyboard function

--- Graphics.ino
+++ Graphics.ino
@@ -1,7 +1,35 @@
 #include <ESPVGAX2.h>
 #include <fonts2/monodos8.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
+ESP8266WebServer server;
+const char* ssid     = "ReidWifi";
+const char* password = "Scott 1314";
+
+IPAddress ip(192,168,11,4);
+IPAddress gateway(192,168,11,1);
+IPAddress subnet(255,255,255,0);
 ESPVGAX2 vga;
+
+char webpage[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Keyboard</title>
+<style>
+</style>
+</head>
+<body>
+</body>
+<script>
+</script>
+</html>
+)=====";
+
 char matrix[60][64];
 String text = "";
 bool f = true;
@@ -28,7 +56,14 @@ void loop() {
 }
 void setup() { 
   Serial.begin(9600);      
+  
   vga.begin();
   vga.setBitmapFont((uint8_t*)img2_monodos8_data, 8);
   memset(matrix, ' ', sizeof matrix);
+
+  WiFi.softAPConfig(ip, gateway, subnet);
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+
+  server.on("/",[](){server.send(200,"text/html", webpage);});
 }
